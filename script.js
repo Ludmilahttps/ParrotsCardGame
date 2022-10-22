8d81ebb71888dc4bbeca076b3f3c5465468e840c
Ludmilahttps <ludmila.https@gmail.com>
2022-10-22 18:07:15 -0300

    add cards

--- script.js
+++ script.js
@@ -1,30 +1,50 @@
-document.addEventListener("keypress", function(e) {
-    if(e.key === 'Enter') {
+const BackCards = [
+    "bobrossparrot.gif",
+    "explodyparrot.gif",
+    "fiestaparrot.gif",
+    "metalparrot.gif",
+    "revertitparrot.gif",
+    "tripletsparrot.gif",
+    "unicornparrot.gif",
+];
+
+document.addEventListener("keypress", function (e) {
+    if (e.key === 'Enter') {
         var btn = document.querySelector("#submit");
         btn.click();
         start();
-    } 
-  });
+    }
+});
 
-function start()
-{
+function start() {
+    const section = document.querySelector('.section');
+
     const aux = document.querySelector('.begin');
     aux.classList.remove('begin');
 
     let input = document.querySelector("#value");
     let value = input.value;
 
-    for(let i=0; i<=value; i++)
-    {
-        Selection.inerHTML = Selection.inerHTML + `
-        <card>
-            <img src="assets/back.png" onclick="TurnCard()" class="back">
+    for (let i = 0; i < value; i++) {
+        section.innerHTML += `
+        <card onclick="TurnCard(this)">
+            <div class="back">
+                <img src="assets/back.png" class="back-card">
+            </div>
+            <div class="front none">
+                <img src="assets/${BackCards[i]}" class="front-card" />
+            </div>
         </card>`;
     }
 }
 
-function TurnCard()
-{
-    const aux = document.querySelector('.back');
-    aux.classList.remove('begin');
+function TurnCard(card) {
+    let back = card.querySelector('.back');
+    let none = card.querySelector('.none');
+
+    if (back != none) {
+        none.classList.remove('none');
+        back.classList.add('none');
+    }
+
 }
