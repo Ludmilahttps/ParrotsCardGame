5f5281d448d940c1bb873f831d5713b07b39d75d
Ludmilahttps <ludmila.https@gmail.com>
2022-10-23 22:56:22 -0300

    salta win and lose

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const FrontCards = [
     "unicornparrot.gif",
 ];
 
+let flipped = 0;
+let first, second;
+
 document.addEventListener("keypress", function (e) {
     if (e.key === 'Enter') {
         var btn = document.querySelector("#submit");
@@ -24,17 +27,28 @@ function start() {
 
     let input = document.querySelector("#value");
     let value = input.value;
+    var cards = [];
 
     if (value % 2 == 0) {
 
+        for (let j = 0; j < (value / 2); j++) {
+            cards[j] = FrontCards[j];
+        }
+
+        for (let l = 0; l < (value / 2); l++) {
+            cards[(value / 2) + l] = FrontCards[l];
+        }
+
+        cards = shurffle(cards);
+
         for (let i = 0; i < value; i++) {
             section.innerHTML += `
-        <card onclick="TurnCard(this)">
+        <card onclick="TurnCard(this)" id="${cards[i]}">
             <div class="back">
                 <img src="assets/back.png" class="back-card">
             </div>
             <div class="front none">
-                <img src="assets/${FrontCards[i]}" class="front-card" />
+                <img src="assets/${cards[i]}" class="front-card" />
             </div>
         </card>`;
         }
@@ -44,20 +58,68 @@ function start() {
     }
 }
 
-function shurffle() {
-    cards.forEach(card => {
-        let ramdomPos = Math.floor(Math.random() * 12);
-        card.style.order = ramdomPos;
-    });
+function shurffle(cards) {
+    for (let i = cards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [cards[i], cards[j]] = [cards[j], cards[i]];
+    }
+    return cards;
 }
 
 function TurnCard(card) {
     let back = card.querySelector('.back');
     let none = card.querySelector('.none');
+    let First = '', Second = '' ;
 
-    if (back != none) {
+    if (flipped === 0)//se nenhuma carta estiver virada
+    {
+        card.classList.toggle("turn");
         none.classList.remove('none');
         back.classList.add('none');
+        flipped = 1;
+        card.classList.add('first');
+        First = card;
     }
+    else {
+        card.classList.toggle("turn");
+        none.classList.remove('none');
+        back.classList.add('none');
+        card.classList.add('second');
+        Second = card;
 
+        if (First.innerHTML == Second.innerHTML) //se a imagem da primeira e a imagem da segunda forem iguais
+        {
+            win(First, Second);
+        }
+        else {
+            lose(First, Second);
+        }
+    }
+}
+
+function win(first, second) {
+    alert("You win");
+    let front = first.querySelector('.front');
+    let none = second.querySelector('.none');
+}
+
+function lose(first, second) {
+
+    alert("You lose");
+
+    let front = first.querySelector('.front');
+    let none = first.querySelector('.none');
+    first.classList.toggle("turn");
+    none.classList.remove('none');
+    front.classList.add('none');
+    first.classList.remove('first');
+
+    front = second.querySelector('.front');
+    none = second.querySelector('.none');
+    second.classList.toggle("turn");
+    none.classList.remove('none');
+    front.classList.add('none');
+    second.classList.remove('second');
+
+    flipped == 0;
 }
